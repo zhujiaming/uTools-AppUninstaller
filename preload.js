588d3c04c7ef95646036a8ecf625ac7a75e04b47
fofolee <fofolee@qq.com>
2019-03-28 15:55:55 +0800

    v0.0.1

---
+++ preload.js
@@ -0,0 +1,84 @@
+const fs = require('fs');
+const path = require("path");
+const iconExtractor = require('icon-extractor');
+const os = require('os')
+const PowerShell = require("powershell");
+const { exec } = require('child_process')
+const iconv = require('iconv-lite')
+
+
+getico = apps =>{
+    iconExtractor.emitter.on('icon', function (data) {
+        let icondir = path.join(os.tmpdir(), 'ProcessIcon')
+        fs.exists(icondir, exists => {
+            if (!exists) { fs.mkdirSync(icondir) }
+            let iconpath = path.join(icondir, `${data.Context}.png`)
+            fs.exists(iconpath, exists => {
+                if (!exists) {
+                    fs.writeFile(iconpath, data.Base64ImageData, "base64", err => {
+                        if (err) { console.log(err); }
+                    })
+                }
+            })
+        })
+    });
+
+    for (var app of apps) {
+        if (app.DisplayIcon != undefined) {
+            app.DisplayIcon = app.DisplayIcon.split(',')[0];
+        }
+        iconExtractor.getIcon(app.LegalName, app.DisplayIcon);
+    }
+}
+
+applist = (callback) => {
+    let filterValues = "Select-Object DisplayName,DisplayIcon,UninstallString,DisplayVersion,InstallDate,Publisher"
+    let localMatcine = `Get-ItemProperty HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\* | ${filterValues}`;
+    let currentUser = `Get-ItemProperty HKCU:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\* | ${filterValues}`;
+    let Wow6432Node = `Get-ItemProperty HKLM:\\SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\* | ${filterValues}`;
+    if (process.arch == 'x64') {
+        var ps = new PowerShell(`chcp 65001;${localMatcine};${currentUser};${Wow6432Node}`);      
+    } else {
+        var ps = new PowerShell(`chcp 65001;${localMatcine};${currentUser}`);              
+    }
+    ps.on("output", data => {
+        let applist = [];
+        let apps = data.trim().replace(/\r\n[ ]{10,}/g,"").split('\r\n\r\n');
+        for (var app of apps) {
+            dict = {}
+            let lines = app.split('\r\n')
+            for (var line of lines) {
+                if (line) {
+                    let key = line.split(/\s+:\s*/)[0];
+                    let value = line.split(/\s+:\s*/)[1];
+                    dict[key] = value;
+                }
+            }
+            if (dict.DisplayName) {
+                dict.LegalName = dict.DisplayName.replace(/[\\\/\:\*\?\"\<\>\|]/g, "");
+                var icon = path.join(os.tmpdir(), 'ProcessIcon', `${encodeURIComponent(dict.LegalName)}.png`);
+                dict.Icon = icon
+                applist.push(dict);
+            }
+        }
+        applist.shift();
+        getico(applist);
+        callback(applist);
+    });
+}
+
+appremove = (command, callback) => {
+    command = command.replace(/(^[A-z]:\\[\S ]+\\\S+)($| )/, '"$1"$2')
+    exec(command, { encoding : 'buffer' }, (err, stdout, stderr) => {
+        if (err) {
+            callback(iconv.decode(stderr, 'cp936'));
+        }
+    })
+}
+
+
+applist(apps => {
+    for (var app of apps) {
+        console.log(app.UninstallString);
+    }
+})
---
+++ readme.md
@@ -0,0 +1,39 @@
+# 软件卸载 AppUninstaller V0.0.1
+
+### 简介
+
+从注册表读取当前安装的软件，并选择卸载，比控制面板列出的软件要多
+
+### 功能
+
+- 列出注册表中有的所有安装的软件
+- 在输入框进行软件筛选
+- 直接回车卸载列表中的第一个软件
+- 点击卸载相应的软件
+- 不支持 macOS
+
+### 预览
+
+![usage.gif](https://i.loli.net/2019/03/28/5c9c74e5e4d7f.gif)
+
+
+
+### 下载
+
+[百度网盘](https://pan.baidu.com/s/1yx6T-ElEDs-T7o38qFs9xA) 提取码: `xh3y`
+
+[项目地址](https://github.com/fofolee/uTools-AppUninstaller/)
+
+[插件发布页](https://yuanliao.info/d/317)
+
+### 安装方法
+
+将`asar`文件拖入`uTools`输入框中安装即可，需要重启`uTools`生效
+
+### 关键字
+
+`uninstall` `卸载`
+
+### ~~Todo~~ (Maydo)
+
+- 快捷键或方向键对列表进行选择
